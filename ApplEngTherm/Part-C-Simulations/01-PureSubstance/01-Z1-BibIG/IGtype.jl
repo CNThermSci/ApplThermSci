70673c9403b8a96aaa670045cb905d01c2eb63aa
C Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2020-09-09 22:44:58 -0300

    UPD on the IGtype.jl

--- ApplEngTherm/Part-C-Simulations/01-PureSubstance/01-Z1-BibIG/IGtype.jl
+++ ApplEngTherm/Part-C-Simulations/01-PureSubstance/01-Z1-BibIG/IGtype.jl
@@ -1,7 +1,15 @@
 # Universal gas constant
 R̄() = 8.314472 # ± 0.000015 # kJ/kmol·K
 
-# Ideal Gas structure
-struct IG
+# Standard Tref
+Tref() = 298.15 # K
 
+# IG (Ideal Gas) structure: values for each gas instance
+struct IG
+    MW                  # Molecular "Weight", kg/kmol
+    CP::Ntuple{4}       # Exactly 4 c̄p(T) coefficients
+    Tmin                # T_min, K
+    Tmax                # T_max, K
+    sref                # s̄°ref, kJ/kmol·K
 end
+

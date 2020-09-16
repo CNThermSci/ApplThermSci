64534bad65b628090826a2bd847397e33bf1c2ec
C Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2020-09-15 23:15:32 -0300

    UPD the IGlib_0.jl through Pluto

--- ApplEngTherm/Part-C-Simulations/01-PureSubstance/01-Z1-BibIG/lib/coding/IGlib_0.jl
+++ ApplEngTherm/Part-C-Simulations/01-PureSubstance/01-Z1-BibIG/lib/coding/IGlib_0.jl
@@ -15,5 +15,14 @@ Biblioteca básica de gás ideal sob hipóteses:
 - sref = s(Tref) conhecida.
 "
 
+# ╔═╡ 3cf7ab10-f7c2-11ea-0386-97c6d1f5ffc5
+md"## Setup Mínimo"
+
+# ╔═╡ 53ea6024-f7c2-11ea-2226-f9d22949c8b7
+# Universal gas constant
+R̄() = 8.314472 # ± 0.000015 # kJ/kmol⋅K
+
 # ╔═╡ Cell order:
 # ╟─e6313090-f7c0-11ea-0f25-5128ff9de54b
+# ╟─3cf7ab10-f7c2-11ea-0386-97c6d1f5ffc5
+# ╠═53ea6024-f7c2-11ea-2226-f9d22949c8b7

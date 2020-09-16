c17b4911dd6d855edc0f95c3160a4414ddea4f25
C Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2020-09-16 00:50:17 -0300

    UPD the IGlib_0.jl through Pluto

--- ApplEngTherm/Part-C-Simulations/01-PureSubstance/01-Z1-BibIG/lib/coding/IGlib_0.jl
+++ ApplEngTherm/Part-C-Simulations/01-PureSubstance/01-Z1-BibIG/lib/coding/IGlib_0.jl
@@ -4,6 +4,9 @@
 using Markdown
 using InteractiveUtils
 
+# ╔═╡ 934ae304-f7ce-11ea-2b06-9b0f48cd9c22
+using CSV
+
 # ╔═╡ e6313090-f7c0-11ea-0f25-5128ff9de54b
 md"# Biblioteca Simplificada de Gás Ideal
 
@@ -42,6 +45,15 @@ md"## Biblioteca Externa
 Valores tabelados em Çengel, Y. A., Termodinâmica 7a Ed. ISBN 978-85-8055-200-3 (Tabelas A-1, A-2 e A-26), foram transportados para uma planilha (OpenOffice Spreadsheet) e exportados no formato CSV [Comma Separated Values], os quais podem ser lidos em Julia:
 "
 
+# ╔═╡ 034e8264-f7cf-11ea-2a5b-b13e84ce9026
+gasLib = CSV.File("IGTable.csv")
+
+# ╔═╡ 04402ca4-f7cf-11ea-02e7-2d95f990f682
+
+
+# ╔═╡ 0411c8a0-f7cf-11ea-15ec-636d951c8e49
+
+
 # ╔═╡ Cell order:
 # ╟─e6313090-f7c0-11ea-0f25-5128ff9de54b
 # ╟─3cf7ab10-f7c2-11ea-0386-97c6d1f5ffc5
@@ -49,3 +61,7 @@ Valores tabelados em Çengel, Y. A., Termodinâmica 7a Ed. ISBN 978-85-8055-200-
 # ╠═815a5db4-f7c2-11ea-1747-e3f2eccdf1b2
 # ╠═8125f198-f7c2-11ea-14e4-7f873ab2c3f4
 # ╟─e66a728a-f7cd-11ea-1fe6-07c915fa1c9d
+# ╠═934ae304-f7ce-11ea-2b06-9b0f48cd9c22
+# ╠═034e8264-f7cf-11ea-2a5b-b13e84ce9026
+# ╠═04402ca4-f7cf-11ea-02e7-2d95f990f682
+# ╠═0411c8a0-f7cf-11ea-15ec-636d951c8e49

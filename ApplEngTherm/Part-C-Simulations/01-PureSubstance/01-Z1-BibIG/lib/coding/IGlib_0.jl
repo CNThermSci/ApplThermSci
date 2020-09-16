a029381703a1e6b2bafbb0b8c237a24a1483d202
C Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2020-09-16 01:18:06 -0300

    UPD the IGlib_0.jl through Pluto

--- ApplEngTherm/Part-C-Simulations/01-PureSubstance/01-Z1-BibIG/lib/coding/IGlib_0.jl
+++ ApplEngTherm/Part-C-Simulations/01-PureSubstance/01-Z1-BibIG/lib/coding/IGlib_0.jl
@@ -46,7 +46,25 @@ Valores tabelados em Çengel, Y. A., Termodinâmica 7a Ed. ISBN 978-85-8055-200-
 "
 
 # ╔═╡ 034e8264-f7cf-11ea-2a5b-b13e84ce9026
-gasDat = CSV.File("IGTable.csv")
+gasRaw = CSV.File("IGTable.csv", normalizenames=true)
+
+# ╔═╡ ad44f412-f7d2-11ea-0524-6f802013e302
+function rowToIG(row)
+	IG( row.MW_kg_kmol, (
+			row.cp_a_kJ_kmol_K,
+			row.cp_b_kJ_kmol_K²,
+			row.cp_c_kJ_kmol_K³,
+			row.cp_d_kJ_kmol_K⁴),
+		row.Tmin_K,
+		row.Tmax_K,
+		row.sref_kJ_kmol_K)
+end
+
+# ╔═╡ 21eb877c-f7d1-11ea-241a-5b5b9166d851
+gasLib = Dict(Symbol(r.Formula) => rowToIG(r) for r in gasRaw)
+
+# ╔═╡ 21ba8f8c-f7d1-11ea-0419-4f1f17f1f76d
+gasLib[:O2]
 
 # ╔═╡ 04402ca4-f7cf-11ea-02e7-2d95f990f682
 md"## Funcionalidade da Biblioteca
@@ -55,6 +73,12 @@ Funções que calculam propriedades termodinâmicas dos gases."
 
 # ╔═╡ 0411c8a0-f7cf-11ea-15ec-636d951c8e49
 md"### Comportamento P-T-v"
+
+# ╔═╡ 00e60032-f7d0-11ea-3784-cd9ef42ea3a6
+
+
+# ╔═╡ 0190c5f8-f7d0-11ea-2f9c-f73bf010a371
+
 
 # ╔═╡ Cell order:
 # ╟─e6313090-f7c0-11ea-0f25-5128ff9de54b
@@ -65,5 +89,10 @@ md"### Comportamento P-T-v"
 # ╟─e66a728a-f7cd-11ea-1fe6-07c915fa1c9d
 # ╠═934ae304-f7ce-11ea-2b06-9b0f48cd9c22
 # ╠═034e8264-f7cf-11ea-2a5b-b13e84ce9026
+# ╠═ad44f412-f7d2-11ea-0524-6f802013e302
+# ╠═21eb877c-f7d1-11ea-241a-5b5b9166d851
+# ╠═21ba8f8c-f7d1-11ea-0419-4f1f17f1f76d
 # ╟─04402ca4-f7cf-11ea-02e7-2d95f990f682
 # ╟─0411c8a0-f7cf-11ea-15ec-636d951c8e49
+# ╠═00e60032-f7d0-11ea-3784-cd9ef42ea3a6
+# ╠═0190c5f8-f7d0-11ea-2f9c-f73bf010a371

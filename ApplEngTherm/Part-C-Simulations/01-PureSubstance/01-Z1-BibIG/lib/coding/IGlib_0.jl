d0bd422534c919c22bdc17b713cf20e406d6f7ca
C Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2020-09-16 00:41:41 -0300

    UPD the IGlib_0.jl through Pluto

--- ApplEngTherm/Part-C-Simulations/01-PureSubstance/01-Z1-BibIG/lib/coding/IGlib_0.jl
+++ ApplEngTherm/Part-C-Simulations/01-PureSubstance/01-Z1-BibIG/lib/coding/IGlib_0.jl
@@ -36,9 +36,16 @@ struct IG
     sref                # s̄°ref, kJ/kmol·K
 end
 
+# ╔═╡ e66a728a-f7cd-11ea-1fe6-07c915fa1c9d
+md"## Biblioteca Externa
+
+Valores tabelados em Çengel, Y. A., Termodinâmica 7a Ed. ISBN 978-85-8055-200-3 (Tabelas A-1, A-2 e A-26), foram transportados para uma planilha (OpenOffice Spreadsheet) e exportados no formato CSV [Comma Separated Values], os quais podem ser lidos em Julia:
+"
+
 # ╔═╡ Cell order:
 # ╟─e6313090-f7c0-11ea-0f25-5128ff9de54b
 # ╟─3cf7ab10-f7c2-11ea-0386-97c6d1f5ffc5
 # ╠═53ea6024-f7c2-11ea-2226-f9d22949c8b7
 # ╠═815a5db4-f7c2-11ea-1747-e3f2eccdf1b2
 # ╠═8125f198-f7c2-11ea-14e4-7f873ab2c3f4
+# ╟─e66a728a-f7cd-11ea-1fe6-07c915fa1c9d

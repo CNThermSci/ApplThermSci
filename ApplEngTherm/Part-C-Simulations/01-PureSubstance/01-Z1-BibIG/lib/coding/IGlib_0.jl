a8d663528c7741e4a646dbe0bbd02ce63a0195d1
C Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2020-09-15 23:16:10 -0300

    UPD the IGlib_0.jl through Pluto

--- ApplEngTherm/Part-C-Simulations/01-PureSubstance/01-Z1-BibIG/lib/coding/IGlib_0.jl
+++ ApplEngTherm/Part-C-Simulations/01-PureSubstance/01-Z1-BibIG/lib/coding/IGlib_0.jl
@@ -22,7 +22,16 @@ md"## Setup Mínimo"
 # Universal gas constant
 R̄() = 8.314472 # ± 0.000015 # kJ/kmol⋅K
 
+# ╔═╡ 815a5db4-f7c2-11ea-1747-e3f2eccdf1b2
+# Standard Tref
+Tref() = 298.15 # K
+
+# ╔═╡ 8125f198-f7c2-11ea-14e4-7f873ab2c3f4
+
+
 # ╔═╡ Cell order:
 # ╟─e6313090-f7c0-11ea-0f25-5128ff9de54b
 # ╟─3cf7ab10-f7c2-11ea-0386-97c6d1f5ffc5
 # ╠═53ea6024-f7c2-11ea-2226-f9d22949c8b7
+# ╠═815a5db4-f7c2-11ea-1747-e3f2eccdf1b2
+# ╠═8125f198-f7c2-11ea-14e4-7f873ab2c3f4

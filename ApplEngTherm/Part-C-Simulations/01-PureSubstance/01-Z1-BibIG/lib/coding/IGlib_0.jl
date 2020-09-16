9aafbf04f09bcddbe32e1d4f4e99de3e88b06fc9
C Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2020-09-15 23:13:24 -0300

    UPD the IGlib_0.jl through Pluto

--- ApplEngTherm/Part-C-Simulations/01-PureSubstance/01-Z1-BibIG/lib/coding/IGlib_0.jl
+++ ApplEngTherm/Part-C-Simulations/01-PureSubstance/01-Z1-BibIG/lib/coding/IGlib_0.jl
@@ -5,7 +5,15 @@ using Markdown
 using InteractiveUtils
 
 # ╔═╡ e6313090-f7c0-11ea-0f25-5128ff9de54b
-md"# Biblioteca de Gás Ideal"
+md"# Biblioteca Simplificada de Gás Ideal
+
+Biblioteca básica de gás ideal sob hipóteses:
+
+- Substâncias puras;
+- Função cp(T) polinomial cúbica;
+- Função cp(T) válida para Tmin ⩽ T ⩽ Tmax;
+- sref = s(Tref) conhecida.
+"
 
 # ╔═╡ Cell order:
-# ╠═e6313090-f7c0-11ea-0f25-5128ff9de54b
+# ╟─e6313090-f7c0-11ea-0f25-5128ff9de54b

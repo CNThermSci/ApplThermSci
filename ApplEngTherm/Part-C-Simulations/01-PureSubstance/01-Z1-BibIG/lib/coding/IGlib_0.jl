f5850aedbef7d706de86d4449e1936000458278d
C Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2020-09-15 23:05:36 -0300

    ADD the IGlib_0.jl Pluto notebook

---
+++ ApplEngTherm/Part-C-Simulations/01-PureSubstance/01-Z1-BibIG/lib/coding/IGlib_0.jl
@@ -0,0 +1,11 @@
+### A Pluto.jl notebook ###
+# v0.11.12
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ e6313090-f7c0-11ea-0f25-5128ff9de54b
+
+
+# ╔═╡ Cell order:
+# ╠═e6313090-f7c0-11ea-0f25-5128ff9de54b

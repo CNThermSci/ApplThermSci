3ceb1bb0bd4505109335b5f98e4eab71c8fe8d95
C Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2020-10-20 22:14:54 -0300

    UPD working on slides-merge.sh

--- ApplEngTherm/Part-X-Congress/20/01-ENCIT2020-0067/slides-merge.sh
+++ ApplEngTherm/Part-X-Congress/20/01-ENCIT2020-0067/slides-merge.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
+# Background slide for composition
+atop=${1:-Slide2.png}
+
 # Constants
+PRE='X2001-en-PRES'
 geo='+486+174'
-dir='X2001-en-PRES.png'
+
+# Functions
+function getFrames(){
+    echo ${PRE}*png | sed "s|${PRE}-||g;s|\.png||g"
+}
 
 # Execution
-cd $dir
+cd "${PRE}.png"
+for _F in $(getFrames); do
+    _f=$(printf "%03d" $_F)
+    composite \
+        -geometry "${geo}" \
+        ./${PRE}-${_F}.png \
+        ../${atop} \
+        ./slide-${_f}.png
+done
 cd -
 

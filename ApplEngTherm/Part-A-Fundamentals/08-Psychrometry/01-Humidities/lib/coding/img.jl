14f358eac1e97cb47eb205f9b2b59dcd824af60f
C Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2021-02-10 20:34:34 -0300

    UPD lib/coding/img.jl through Pluto.jl

--- ApplEngTherm/Part-A-Fundamentals/08-Psychrometry/01-Humidities/lib/coding/img.jl
+++ ApplEngTherm/Part-A-Fundamentals/08-Psychrometry/01-Humidities/lib/coding/img.jl
@@ -5,7 +5,13 @@ using Markdown
 using InteractiveUtils
 
 # ╔═╡ b31badd0-6bf7-11eb-266b-cb57b725746e
+md"""# Pixels e Imagens
+Básico de cores e composição de imagens via dados em `julia`.
+"""
+
+# ╔═╡ 69f94786-6bf8-11eb-154c-d18b8c8fe404
 
 
 # ╔═╡ Cell order:
-# ╠═b31badd0-6bf7-11eb-266b-cb57b725746e
+# ╟─b31badd0-6bf7-11eb-266b-cb57b725746e
+# ╠═69f94786-6bf8-11eb-154c-d18b8c8fe404

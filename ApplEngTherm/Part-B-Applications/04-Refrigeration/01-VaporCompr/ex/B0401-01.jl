bfb39ecc30aed10ebcf5a4462d911fbb1e4e83cc
C Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2021-03-19 18:57:05 -0300

    UPD the ex/B0401-01.jl

--- ApplEngTherm/Part-B-Applications/04-Refrigeration/01-VaporCompr/ex/B0401-01.jl
+++ ApplEngTherm/Part-B-Applications/04-Refrigeration/01-VaporCompr/ex/B0401-01.jl
@@ -19,7 +19,6 @@ begin
 	using PyCall
 	CP = pyimport("CoolProp.CoolProp")
 	using Printf
-	using Plots
 end
 
 # ╔═╡ 570e1fe8-7206-11eb-29d3-e1b68516ba96
@@ -146,79 +145,16 @@ md"""
 ### Bibliotecas
 """
 
-# ╔═╡ f96141a6-71f9-11eb-065a-69b0f4594921
-md"""
-### Gráfico - Domo de Saturação da Água
-"""
-
-# ╔═╡ b4b97260-717e-11eb-25c3-ffcd02a2662e
-begin
-	# Basic constants
-	FL = "water"
-	TT = CP.PropsSI("Ttriple", FL)
-	TC = CP.PropsSI("Tcrit", FL)
-	TSTL = (TT, CP.PropsSI("S", "T", TT, "Q", 0.0, FL))
-	TSTV = (TT, CP.PropsSI("S", "T", TT, "Q", 1.0, FL))
-	TSCR = (TC, CP.PropsSI("S", "T", TC, "Q", 0.5, FL))
-	# Saturation dome
-	N = range(0.0, stop=1.0, length=20)
-	P = 1.0 .- (1.0 .- N).^2
-	tLiq = TT .+ (TC - TT) .* P
-	tVap = reverse(tLiq)[2:end]
-	sLiq = [CP.PropsSI("S", "T", _t, "Q", 0.0, FL) for _t in tLiq] .* 1.0e-3
-	sVap = [CP.PropsSI("S", "T", _t, "Q", 1.0, FL) for _t in tVap] .* 1.0e-3
-	DOME = (cat(tLiq, tVap, dims=1).-273.15, cat(sLiq, sVap, dims=1))
-end;
-
-# ╔═╡ 98a9cb5c-7187-11eb-08f2-d53ad216d48e
-begin
-	# Isobar @ Pv
-	if Pv > minP
-		_P = plot(DOME[2], DOME[1], lw=3, size=(320, 180), legend=false)
-		PO = CP.PropsSI("T", "P", Pv * 1.0e+3, "Q", 0.0, FL)
-		OL = (PO, CP.PropsSI("S", "P", Pv * 1.0e+3, "Q", 0.0, FL) * 1.0e-3)
-		OV = (PO, CP.PropsSI("S", "P", Pv * 1.0e+3, "Q", 1.0, FL) * 1.0e-3)
-		tIso = range(PO, stop=TC, length=10)[2:end]
-		sIso = [CP.PropsSI("S", "T", _t, "P", Pv * 1.0e+3, FL)
-			for _t in tIso] .* 1.0e-3
-		ISOP = (
-			cat([OL[1], OV[1]], tIso, dims=1).-273.15,
-			cat([OL[2], OV[2]], sIso, dims=1))
-		plot!(_P, ISOP[2], ISOP[1], lw=1)		
-		# Water vapor state
-		if the_ϕ < 100.0
-			plot!(_P,
-				[CP.PropsSI("S", "T", the_T+273.15, "P", Pv*1.0e+3, FL)] .* 1.0e-3,
-				[the_T],
-				marker = (:hexagon, 2, 0.6, :green, stroke(3, 0.2, :black, :dot))
-			)
-		else
-			plot!(_P,
-				[CP.PropsSI("S", "T", the_T+273.15, "Q", 1.0, FL)] .* 1.0e-3,
-				[the_T],
-				marker = (:hexagon, 2, 0.6, :green, stroke(3, 0.2, :black, :dot))
-			)
-		end
-	else
-		_P = plot(DOME[2], DOME[1], lw=3, size=(300, 200), legend=false)
-	end
-	savefig("A0801-01.svg")
-	_P
-end
-
 # ╔═╡ Cell order:
 # ╟─570e1fe8-7206-11eb-29d3-e1b68516ba96
 # ╟─056da550-71fc-11eb-3940-5d1996cf5ec2
 # ╠═6dc92e96-7148-11eb-1cc3-cf2d65e8985b
 # ╟─72413c5a-88f4-11eb-08d2-813542bed0f4
 # ╟─7b557108-88f4-11eb-386c-5de9519fa60a
-# ╠═5a2b3bd6-714b-11eb-0208-5f1b44e7cb4c
+# ╟─5a2b3bd6-714b-11eb-0208-5f1b44e7cb4c
 # ╟─59f6ad1c-714b-11eb-1b85-0542622b8aba
 # ╠═32a25170-88f8-11eb-2b1a-a74304a5c40d
 # ╟─9f4f1ef4-88fb-11eb-0b47-0568605b0400
-# ╟─98a9cb5c-7187-11eb-08f2-d53ad216d48e
 # ╟─96c9b986-717e-11eb-21d0-5d3bdcdaf318
 # ╟─0a3b27a8-71fa-11eb-32c4-517738939197
 # ╠═44780316-7149-11eb-2c22-91b75023501a
-# ╟─f96141a6-71f9-11eb-065a-69b0f4594921
-# ╠═b4b97260-717e-11eb-25c3-ffcd02a2662e
